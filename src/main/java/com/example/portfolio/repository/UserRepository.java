bd91e6432bcbf39c185062e6a49e845f524ec51b
MansiBora11 <boramansi118@gmail.com>
2025-05-06 23:28:29 +0530

    Remove outdated src folder from root

--- src/main/java/com/example/portfolio/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.portfolio.repository;
-
-import com.example.portfolio.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
